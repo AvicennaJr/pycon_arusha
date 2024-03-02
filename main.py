4fefdc299ac6c326989f5fc5e8c69ecd2757a79f
Fuad <justfuad@protonmail.com>
2024-03-02 13:56:45 +0300

    first application - without memory

---
+++ main.py
@@ -0,0 +1,25 @@
+from groq import Groq
+import gradio as gr
+
+client = Groq(api_key="",)
+
+def chat(message):
+    chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": message,
+        }
+    ],
+    model="mixtral-8x7b-32768",
+    )
+
+    return chat_completion.choices[0].message.content
+
+demo = gr.Interface(
+    fn=chat,
+    inputs=["text"],
+    outputs=["text"],
+)
+
+demo.launch()
